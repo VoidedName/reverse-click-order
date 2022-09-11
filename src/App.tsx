541906a935f05658c52bed8c934c118e925958ab
TheLazyBit <daniel.kogan@gmx.net>
2022-09-11 15:57:11 +0200

    implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC, PropsWithChildren, useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+const GridLayout: FC<PropsWithChildren> = ({children}) => <div className={'grid-layout'}>{children}</div>;
+const GridRow: FC<PropsWithChildren> = ({children}) => <div className={'row'}>{children}</div>;
+const EmptyCell: FC = () => <div className={'cell'} />;
+
+type GridCellProps = {
+  onClick?: () => void,
+  marked?: boolean,
+}
+const GridCell: FC<GridCellProps> = ({onClick, marked}) => <div
+  className={`cell simple-border ${marked ? 'clicked' : ''} ${onClick ? 'clickable' : ''}`}
+  onClick={onClick}
+/>;
+
+
+function cellLabel(x: number, y: number) {
+  return `${x},${y}`;
+}
+
+const isMarked = (x: number, y: number, marked: string[]) => marked.includes(cellLabel(x, y));
+
+const ReversableButtonGrid: FC<{ grid: boolean[][], animationSpeed?: number }> = ({grid, animationSpeed = 250}) => {
+  const [clickOrder, setClickOrder] = useState<string[]>([]); // "x,y" <- id shape
+  const [playingBack, setPlayingBack] = useState(false);
+
+  const markCell = (x: number, y: number) => setClickOrder(prev => isMarked(x, y, prev) ? prev : [...prev, cellLabel(x, y)]);
+  const allCellsMarked = clickOrder.length >= grid.reduce((sum, row) => sum + row.filter(cell => cell).length, 0);
+
+  useEffect(() => {
+    if (allCellsMarked) {
+      setPlayingBack(true);
+    }
+    if (clickOrder.length === 0) {
+      setPlayingBack(false);
+    }
+  }, [allCellsMarked, clickOrder]);
+
+  useEffect(() => {
+    if (playingBack) {
+      const timer = setTimeout(() => {
+        setClickOrder(prev => prev.slice(1));
+      }, animationSpeed);
+      return () => clearTimeout(timer);
+    }
+  }, [playingBack, clickOrder, animationSpeed]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GridLayout>
+      {grid.map((row, y) =>
+        <GridRow key={y}>
+          {row.map((cell, x) => cell
+            ? <GridCell
+              key={x}
+              marked={isMarked(x, y, clickOrder)}
+              onClick={playingBack || isMarked(x, y, clickOrder) ? undefined : () => markCell(x,y)}
+            />
+            : <EmptyCell key={x} />
+          )}
+        </GridRow>)}
+    </GridLayout>
   );
+};
+
+function App() {
+  return <>
+    <ReversableButtonGrid grid={[
+      [true, true, true],
+      [false, true, false],
+      [true, true, true],
+    ]} />
+  </>;
 }
 
 export default App;
